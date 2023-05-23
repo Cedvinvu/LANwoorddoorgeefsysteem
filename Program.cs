b386fbb49865a85372009cec500896b9988edd3a
Cedric VU <114571271+Cedvinvu@users.noreply.github.com>
2023-05-23 18:17:27 +0200

    cleaning code

--- Program.cs
+++ Program.cs
@@ -9,14 +9,15 @@ namespace NietGrappigNetwerkDing
     {
         static string tekst;
         static string naam;
-        static string recipientIP;
+        static string IPaanvanger;
+        static int port = 13000;
 
         static void Main(string[] args)
         {
             var menu = new SMUtils.Menu();
 
-            menu.AddOption('1', "Stuur een bericht", SendMessage);
-            menu.AddOption('2', "Vang een bericht", ReceiveMessage);
+            menu.AddOption('1', "Stuur een bericht", SendMessageOption);
+            menu.AddOption('2', "Vang een bericht", ReceiveMessageOption);
             menu.AddOption('3', "Voeg wat toe aan de lijn", AddToMessageLine);
             menu.AddOption('4', "Verander bericht", ChangeMessage);
             menu.AddOption('5', "Verander IP", ChangeIP);
@@ -25,37 +26,25 @@ namespace NietGrappigNetwerkDing
         }
 
 
-        static void SendMessage()
+        static void SendMessage(string message)
         {
             try
             {
-                //Console.WriteLine("Geef de IP waarnaar je iets wil sturen");
-                // Define the recipient's IP address and port
-                //string recipientIP = Console.ReadLine(); // Change to the recipient's IP address
-                int port = 13000; // Change to the recipient's port
-
-                // Create a TCP/IP socket
-                using (Socket senderSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+                using (Socket senderSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)) // Create a TCP/IP socket
                 {
                     Console.WriteLine("Connecteren...");
-                    // Connect to the recipient
-                    senderSocket.Connect(IPAddress.Parse(recipientIP), port);
+                    senderSocket.Connect(IPAddress.Parse(IPaanvanger), port); // Connect to the recipient
                     Console.WriteLine("Geconnecteerd!");
-                    // Prompt for the message to send
-                    //Console.Write("Enter a message: ");
-                    //string message = Console.ReadLine();
 
-                    string message = naam + ": " + tekst;
+                    if (message == null) message = naam + ": " + tekst;
+                    else message += Environment.NewLine + naam + ": " + tekst;
 
-                    // Convert the message to bytes
-                    byte[] messageBytes = Encoding.ASCII.GetBytes(message);
+                    byte[] messageBytes = Encoding.ASCII.GetBytes(message); // Convert the message to bytes
 
-                    // Send the message to the recipient
-                    senderSocket.Send(messageBytes);
+                    senderSocket.Send(messageBytes); // Send the message to the recipient
                     Console.WriteLine("Message send!");
 
-                    // Close the socket
-                    senderSocket.Shutdown(SocketShutdown.Both);
+                    senderSocket.Shutdown(SocketShutdown.Both); // Close the socket
                     senderSocket.Close();
                 }
             }
@@ -68,38 +57,29 @@ namespace NietGrappigNetwerkDing
             Console.ReadLine();
         }
 
-        static void ReceiveMessage()
+        static string ReceiveMessage(bool showmessage)
         {
             try
             {
-                int port = 13000; // Change to the listening port
-
-                // Create a TCP/IP socket
-                using (Socket receiverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+                using (Socket receiverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)) // Create a TCP/IP socket
                 {
-                    // Bind the socket to the listening IP address and port
-                    receiverSocket.Bind(new IPEndPoint(IPAddress.Any, port));
+                    receiverSocket.Bind(new IPEndPoint(IPAddress.Any, port)); // Bind the socket to the listening IP address and port
 
-                    // Start listening for incoming connections
-                    receiverSocket.Listen(1);
-
+                    receiverSocket.Listen(1); // Start listening for incoming connections
                     Console.WriteLine("Waiting for a connection...");
 
-                    // Accept the incoming connection
-                    Socket clientSocket = receiverSocket.Accept();
-
+                    Socket clientSocket = receiverSocket.Accept(); // Accept the incoming connection
                     Console.WriteLine("Connection accepted.");
-
-                    // Receive the message from the sender
-                    byte[] buffer = new byte[1024];
+ 
+                    byte[] buffer = new byte[1024]; // Receive the message from the sender
                     int bytesRead = clientSocket.Receive(buffer);
                     string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
 
-                    Console.WriteLine("Received message:" + Environment.NewLine + message);
+                    if (showmessage) Console.WriteLine("Received message:" + Environment.NewLine + message); // Display the message
 
-                    // Close the client socket
-                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Shutdown(SocketShutdown.Both); // Close the client socket
                     clientSocket.Close();
+                    return message;
                 }
             }
             catch (Exception e)
@@ -109,71 +89,23 @@ namespace NietGrappigNetwerkDing
 
             Console.WriteLine("Press Enter to exit...");
             Console.ReadLine();
+            return null;
+        }
+
+        static void SendMessageOption()
+        {
+            SendMessage(null);
+        }
+
+        static void ReceiveMessageOption()
+        {
+            ReceiveMessage(false);
         }
 
         static void AddToMessageLine()
         {
-            try
-            {
-                //Console.WriteLine("Geef de IP waarnaar je iets wil sturen");
-                // Define the recipient's IP address and port
-                //string recipientIP = Console.ReadLine(); // Change to the recipient's IP address
-                int port = 13000;
-
-                // Create a TCP/IP socket
-                using (Socket receiverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
-                {
-                    // Bind the socket to the listening IP address and port
-                    receiverSocket.Bind(new IPEndPoint(IPAddress.Any, port));
-
-                    // Start listening for incoming connections
-                    receiverSocket.Listen(1);
-
-                    Console.WriteLine("Waiting for a connection...");
-
-                    // Accept the incoming connection
-                    Socket clientSocket = receiverSocket.Accept();
-
-                    Console.WriteLine("Connection accepted.");
-
-                    // Receive the message from the sender
-                    byte[] buffer = new byte[1024];
-                    int bytesRead = clientSocket.Receive(buffer);
-                    string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-
-                    //Console.WriteLine("Voeg wat toe aan het bericht");
-                    //message += Console.ReadLine();
-
-                    message += Environment.NewLine + naam + ": " + tekst;
-
-                    using (Socket senderSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
-                    {
-                        Console.WriteLine("Connecteren...");
-                        // Connect to the recipient
-                        senderSocket.Connect(IPAddress.Parse(recipientIP), port);
-                        Console.WriteLine("Geconnecteerd!");
-                        // Convert the message to bytes
-                        byte[] messageBytes = Encoding.ASCII.GetBytes(message);
-
-                        // Send the message to the recipient
-                        senderSocket.Send(messageBytes);
-                        Console.WriteLine("Message send!");
-
-                        // Close the socket
-                        senderSocket.Shutdown(SocketShutdown.Both);
-                        senderSocket.Close();
-                    }
-
-                    // Close the client socket
-                    clientSocket.Shutdown(SocketShutdown.Both);
-                    clientSocket.Close();
-                }
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Exception: " + e.Message);
-            }
+            string message = ReceiveMessage(false);
+            SendMessage(message);
 
             Console.WriteLine("Press Enter to exit...");
             Console.ReadLine();
@@ -187,7 +119,7 @@ namespace NietGrappigNetwerkDing
         static void ChangeIP()
         {
             Console.WriteLine("Geef IP:");
-            recipientIP = Console.ReadLine();
+            IPaanvanger = Console.ReadLine();
         }
         static void ChangeName()
         {
